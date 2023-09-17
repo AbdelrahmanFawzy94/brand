7d8a928bc5bc5ae2bf5aaf0fe6b324cab038024b
AbdelrahmanFawzy <afawzy.dev@gmail.com>
2023-09-17 06:44:53 +0300

    remove index files

--- src/app/@core/index.ts
+++
@@ -1,7 +0,0 @@
-// services
-export * from './translation/translationLoader';
-export * from './translation/translation-loader.service';
-export * from './services/localStorage.service';
-
-// helpers
-export * from './apis-urls';
