6b7fcfb67527d0f036ff04f6533940fc74546383
AbdelrahmanFawzy <afawzy.dev@gmail.com>
2023-09-17 06:11:36 +0300

    adding localStorage service

--- src/app/@core/index.ts
+++ src/app/@core/index.ts
@@ -1,6 +1,7 @@
 // services
 export * from './translation/translationLoader';
 export * from './translation/translation-loader.service';
+export * from './services/localStorage.service';
 
 // helpers
 export * from './apis-urls';
---
+++ src/app/@core/services/localStorage.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { environment } from '@env/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalStorageService {
+  constructor() {}
+
+  getItem(key: string): any | null {
+    return JSON.parse(localStorage.getItem(`${environment.applicationName}-${key}`)!);
+  }
+
+  setItem(key: string, value: any): void {
+    localStorage.setItem(`${environment.applicationName}-${key}`, JSON.stringify(value));
+  }
+
+  removeItem(key: string): void {
+    localStorage.removeItem(`${environment.applicationName}-${key}`);
+  }
+
+  clearAll(): void {
+    localStorage.clear();
+  }
+
+  itemsLength(): number {
+    return localStorage.length;
+  }
+}
