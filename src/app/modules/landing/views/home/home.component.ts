bf4627f73047bc22ab81a4ce9ac49314eb081cd7
AbdelrahmanFawzy <afawzy.dev@gmail.com>
2023-11-15 16:56:11 +0300

    update todo

--- src/app/modules/landing/views/home/home.component.ts
+++ src/app/modules/landing/views/home/home.component.ts
@@ -1,13 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+// import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { IconComponent } from '@shared';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    //  TranslateModule,
+    IconComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export default class HomeComponent {
   constructor() {}
+
+  // private _Translate: TranslateService
+
+  // currentLang = 'en';
+  // changeLang() {
+  //   if (this.currentLang === 'en') {
+  //     this._Translate.use('ar');
+  //     this.currentLang = 'ar';
+  //   } else {
+  //     this._Translate.use('en');
+  //     this.currentLang = 'en';
+  //   }
+  // }
 }
