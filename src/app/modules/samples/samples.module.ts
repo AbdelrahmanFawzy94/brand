03a1127a651a6a2fd9d6fc18cdc83886f6e1cf2a
AbdelrahmanFawzy <afawzy.dev@gmail.com>
2024-03-05 22:52:16 +0300

    adding comment

--- src/app/modules/samples/samples.module.ts
+++ src/app/modules/samples/samples.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// TODO remove and put it in app module if we can
+import { HttpClientModule } from '@angular/common/http';
 
 import { SamplesRoutingModule } from './samples-routing.module';
-
-import { CommonModule } from '@angular/common';
 import { SamplesComponent } from './samples.component';
-import { HttpClientModule } from '@angular/common/http';
+
 import { SharedDashboardComponent } from '@shared';
 
 @NgModule({
