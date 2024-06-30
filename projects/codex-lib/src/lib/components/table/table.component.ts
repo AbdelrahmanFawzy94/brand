a0551f6a844881d52b256a50cea186528175bdb6
Abdelrahman F. Elgarsh <aelgarsh.c@solutions.com.sa>
2024-06-30 09:40:16 +0300

    cleanup

--- projects/codex-lib/src/lib/components/table/table.component.ts
+++ projects/codex-lib/src/lib/components/table/table.component.ts
@@ -15,13 +15,14 @@ import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
 export class SharedtableComponent implements OnChanges {
   @ViewChild('paginator', { static: true }) paginator!: Paginator;
   @Input({ required: true }) currentPage!: number;
-
   @Input({ required: true }) data!: any[];
   @Input({ required: true }) columns!: TableColumn[];
+
   @Input() isLoading: boolean = false;
-  @Input() itemsPerPageSelected: number = 5;
   @Input() itemsPerPage: number[] = [5, 10, 20, 30];
+  @Input() itemsPerPageSelected: number = 5;
   @Input() totalItems: number = 0;
+
   @Output() onPage = new EventEmitter<PaginatorState>();
 
   constructor() {}
